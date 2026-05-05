cab0771a15a32f149511c1d61fe61c80b4d4538c
Rezabekfi <filip.rezabek@gmail.com>
2026-05-05 21:07:16 +0200

    tried using data injections and it maybe maybe works

--- App.axaml.cs
+++ App.axaml.cs
@@ -6,26 +6,41 @@ using System.Linq;
 using Avalonia.Markup.Xaml;
 using league_mh_viewer.ViewModels;
 using league_mh_viewer.Views;
+using league_mh_viewer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace league_mh_viewer;
 
 public partial class App : Application
 {
-    public override void Initialize()
+  public override void Initialize()
+  {
+    AvaloniaXamlLoader.Load(this);
+  }
+
+
+  public override void OnFrameworkInitializationCompleted()
+  {
+  
+
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSingleton<IRiotApiService, RiotApiService>();
+    serviceCollection.AddTransient<MainWindowViewModel>();
+
+    var services = serviceCollection.BuildServiceProvider();
+
+    var vm = services.GetRequiredService<MainWindowViewModel>();
+
+
+    if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
     {
-        AvaloniaXamlLoader.Load(this);
+      desktop.MainWindow = new MainWindow
+      {
+          DataContext = vm
+      };
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
-        }
-
-        base.OnFrameworkInitializationCompleted();
-    }
+    base.OnFrameworkInitializationCompleted();
+  }
 }
